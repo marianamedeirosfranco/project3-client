861c64a199e322fc6940a1ed0629f92e69b3a2c4
Mariana Franco <marianamedeirosfranco@gmail.com>
2023-03-13 11:40:40 +0000

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Private from "./components/Private";
+import AddTask from "../src/pages/AddTask";
+import EditTask from "../src/pages/EditTask";
+import Homepage from "../src/pages/Homepage";
+import Login from "../src/pages/Login";
+import Signup from "../src/pages/Signup";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-  
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/tasks"
+          element={
+            <Private>
+              <AddTask />
+              <EditTask />
+            </Private>
+          }
+        />
+        <Route path="/tasks/:id" element={<TaskDetails />} />
+        <Route path="/tasks/new" element={<AddTask />} />
+        <Route path="/tasks/edit/:id" element={<EditTask />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthWrapper } from "./context/auth.context";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
